b3f3d92bb8f0c205536da9e8093aae68713ce895
canicmez <canicmez@gmail.com>
2024-06-02 16:26:21 +0300

    gözdeye gösterdim

---
+++ Lecture1/Problems/bank.py
@@ -0,0 +1,9 @@
+x = input("Greeting: ")
+
+if "hello" in x or "Hello" in x:
+    print("$0")
+elif x[0]=="H" or x[0] == "h":
+    print("$20")
+else:
+    print("$100")
+            
---
+++ Lecture1/Problems/deep.py
@@ -0,0 +1,8 @@
+x = input("What is the answer to the great question of life? ")
+
+if x == "42" or x == "forty-two" or x == "forty two":
+    print("Yes")
+else:
+    print("No")
+
+    
